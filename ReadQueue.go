24b93bf2549b5aad7f8dcb275d400a49d0cf83f5
Hayden Parker <haydenkparker@gmail.com>
2014-10-30 20:34:22 -0700

    Working on file read queue

--- ReadQueue.go
+++ ReadQueue.go
@@ -1,14 +1,54 @@
 package main
 
+import (
+	"os"
+	"io"
+	"fmt"
+	)
+
 type ReadQueue struct {
 	Chunks chan Chunk
+	StaleChunks chan Chunk
+	FileHead int64
+	ChunkSize int
+	ChunkID int
 }
 
-func (queue ReadQueue) NextChunk() {
-	
+func (queue *ReadQueue) Process(file *os.File) {
+	queue.Chunks = make(chan Chunk, 6)
+	queue.StaleChunks = make(chan Chunk, 10)
+	queue.ChunkID = 1
+	file.Seek(queue.FileHead,0)
+	for {
+		for len(queue.StaleChunks) > 0 {
+			queue.Chunks <- (<- queue.StaleChunks)
+		}
+		new_chunk := Chunk{}
+		new_chunk.ID = queue.ChunkID
+		queue.ChunkID++
+		new_chunk_data := make([]byte, queue.ChunkSize)
+		bytes_read, err := file.Read(new_chunk_data)
+		if err == io.EOF {
+			break
+		}
+		if bytes_read < queue.ChunkSize {
+			new_chunk_data = new_chunk_data[:bytes_read]
+		}
+		new_chunk.Data = new_chunk_data
+		queue.Chunks <- new_chunk
+	}
+	close(queue.Chunks)
 }
-
 
 func main() {
-
+	queue := ReadQueue{}
+	queue.ChunkSize = 1
+	f, _ := os.Open("./testfile")
+	queue.Process(f)
+	fmt.Println(<-queue.Chunks)
+	fmt.Println(<-queue.Chunks)
+	fmt.Println(<-queue.Chunks)
+	fmt.Println(<-queue.Chunks)
+	fmt.Println(<-queue.Chunks)
+	fmt.Println(<-queue.Chunks)
 }
