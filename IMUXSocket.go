b7b4edc2cc6f0a81a04080cc8f3786141d401772
Hayden Parker <haydenkparker@gmail.com>
2015-01-14 10:57:28 -0800

    Working on recycling

--- IMUXSocket.go
+++ IMUXSocket.go
@@ -15,16 +15,19 @@ type IMUXSocket struct {
 }
 
 func (imuxsocket *IMUXSocket) Download(buffer Buffer, done chan string) {
+	re_open := false
 	for {
-		// Keep track of transfer speed
+		// For keeping track of the transfer speed
 		start := time.Now()
 		
-		//// if reopen is true
+		// Re-open the socket if it was closed by recycling after the last chunk
+		if re_open {
 		////	get new socket from manager
 		////	if theres an error, break with error
-		////	set reopen false
+			re_open = false
+		}
 		
-		// Get the chunk header from the server
+		// Get the chunk header from the server, 32 byte array containing id and size
 		header_slice = make([]byte, 32)
 		_, err := imuxsocket.Socket.Read(header_slice)
 		if err != nil {
@@ -35,7 +38,7 @@ func (imuxsocket *IMUXSocket) Download(buffer Buffer, done chan string) {
 			break
 		}
 		
-		// Check if the header was all 0s
+		// Check if the header was all 0s indicating no more chunks
 		total := 0
 		for _, data := range header_slice {
 			total += data
@@ -45,7 +48,7 @@ func (imuxsocket *IMUXSocket) Download(buffer Buffer, done chan string) {
 			break
 		}
 		
-		// Parse chunk information and read data
+		// Parse chunk information and read the chunk data
 		header := strings.Fields(string(header_slice))
 		id, _ := strconv.Atoi(header[0])
 		size, _  := strconv.Atoi(header[1])
@@ -59,30 +62,38 @@ func (imuxsocket *IMUXSocket) Download(buffer Buffer, done chan string) {
 			break
 		}
 		
-		// Create chunk and send to buffer
+		// Create a chunk struct and send it to the buffer
 		chunk := Chunk{}
 		chunk.ID = id
 		chunk.Data = chunk_data
 		buffer.Chunks <- chunk
 		
-		//// if recycle
+		// Recycle the socket if needed
+		if imuxmanager.Recycle {
 		////	send the recycling signal
 		////	close the socket
-		////	set reopen to true
+			re_open = true
+		}
 		
-		// Update transfer speed
+		// Update the transfer speed
 		imuxsocket.LastSpeed = time.Since(start)
 	}
+	
+	// Update the speed of the socket to 0 because the transfer is over
+	imuxsocket.LastSpeed = 0
 }
 
 func (imuxsocket *IMUXSocket) Upload(queue ReadQueue, done chan string) {
+	re_open := false
 	for chunk := range queue.Chunks {
 		// Keep track of transfer speed
 		start := time.Now()
 		
-		//// if the socket if marked as closed
-		////	get a new socket from the manager
-		////	mark it as opened
+		// Re-open the socket if recycling closed it
+		if re_open {
+			////	get a new socket from the manager
+			////	mark it as opened
+		}
 		
 		// Create the chunk header containing ID and size
 		header, err := chunk.GenerateHeader()
@@ -115,7 +126,7 @@ func (imuxsocket *IMUXSocket) Upload(queue ReadQueue, done chan string) {
 		
 		//// if request for recycle recieved
 		////	close the socket
-		////	mark the socket as closed
+		////	re_open = true or false
 		
 		// Update transfer speed
 		imuxsocket.LastSpeed = time.Since(start)
@@ -123,6 +134,9 @@ func (imuxsocket *IMUXSocket) Upload(queue ReadQueue, done chan string) {
 	
 	// Write 32 bytes of 0s to indicate there are no more chunks
 	imuxsocket.Socket.Write(make([]byte, 32))
+	
+	// Update the speed of the socket to 0 because the transfer is over
+	imuxsocket.LastSpeed = 0
 }
 
 func (imuxsocket *IMUXSocket) Close() error {
--- IMUXState.go
+++ IMUXState.go
@@ -2,5 +2,5 @@ package main
 
 type IMUXState struct {
 	// ip pairs?  ip: count.  Maybe take a map for this?
-	
+	// indication of client/server for NAT
 }
