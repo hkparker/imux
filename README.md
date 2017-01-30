4ff31d4f88c895e27ae80e62d850fb4d78e1a8ba
Hayden <hayden@hkparker.com>
2017-01-29 21:02:21 -0800

    binary flags, readme update

--- README.md
+++ README.md
@@ -9,18 +9,17 @@ Let's say you wanted to expose an FTP server over imux.
 On the server
 
 ```
-imux -server --listen=0.0.0.0:2121 --dial=localhost:21
+imux -server --listen=0.0.0.0:443 --dial=127.0.0.1:21
 ```
 
 Then on a client, inverse multiplex over 10 sockets
 
 ```
-imux -client --binds={"0.0.0.0": 10} --listen=localhost:21 --dial=server:21
+imux -client --binds={"0.0.0.0": 10} --listen=localhost:21 --dial=server:443
 ```
 
-Now on the client, connect to localhost to FTP to the sever over the imux connection
+The client will use Trust Of First Use (TOFU) and TLS.  Now on the client, connect to localhost to FTP to the sever over the imux connection
 
 ```
 ftp localhost
 ```
-
--- bin/imux/imux.go
+++ bin/imux/imux.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+)
+
+var client bool
+var binds string
+var server bool
+var listen string
+var dial string
+var chunk_size int
 
 func main() {
-	fmt.Println("vim-go")
+	flag.BoolVar(&client, "client", false, "create an imux client")
+	flag.StringVar(&binds, "binds", "{\"0.0.0.0\": 10}", "JSON encoding of map from bind address strings to int counts")
+	flag.BoolVar(&server, "server", false, "create an imux server")
+	flag.StringVar(&listen, "listen", "0.0.0.0:443", "listener address and port for clients to imux out and servers to imux in")
+	flag.StringVar(&dial, "dial", "127.0.0.1:443", "dial address and port for clients to dial servers and servers to dial out")
+	flag.IntVar(&chunk_size, "chunk-size", 16384, "maximum number of bytes per chunk")
+	flag.Parse()
+	validateFlags()
+
+	if server {
+		// create a ManyToOne
+	} else if client {
+		// create a OneToMany
+	}
+}
+
+func validateFlags() {
+
 }
