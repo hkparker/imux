5cd004de8196337a562013c058aa97200680d220
Hayden <hayden@hkparker.com>
2017-01-29 16:49:17 -0800

    added old write queue code

--- write_queue.go
+++ write_queue.go
@@ -6,8 +6,39 @@ import (
 
 type WriteQueue struct {
 	Destination io.Writer
-	TopSequence int
-	Chunks      []Chunk
+	LastDump    int
+	Queue       []Chunk
 }
 
-func (write_queue *WriteQueue) Write(chunk Chunk) {}
+func (write_queue *WriteQueue) Write(chunk Chunk) {
+	write_queue.Insert(chunk)
+	write_queue.Dump()
+}
+
+func (write_queue *WriteQueue) Insert(chunk Chunk) {
+	smaller := 0
+	for _, item := range write_queue.Queue {
+		if item.SequenceID < chunk.SequenceID {
+			smaller++
+		}
+	}
+	smaller_chunks := write_queue.Queue[:smaller]
+	larger_chunks := write_queue.Queue[smaller:]
+	write_queue.Queue = append(smaller_chunks, append([]Chunk{chunk}, larger_chunks...)...)
+}
+
+func (write_queue *WriteQueue) Dump() {
+	for {
+		if len(write_queue.Queue) == 0 {
+			break
+		}
+		chunk := write_queue.Queue[0]
+		if chunk.SequenceID == uint64(write_queue.LastDump+1) {
+			write_queue.Queue = write_queue.Queue[1:]
+			write_queue.Destination.Write(chunk.Data)
+			write_queue.LastDump = write_queue.LastDump + 1
+		} else {
+			break
+		}
+	}
+}
